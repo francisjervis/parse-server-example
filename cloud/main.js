aae1bf6ba1bf7e8445a40b7f6d88541e416c61aa
Francis Jervis <francis@timesfree.co>
2017-01-05 20:42:55 -0800

    Update main.js

--- cloud/main.js
+++ cloud/main.js
@@ -5,29 +5,46 @@ Parse.Cloud.define('hello', function(req, res) {
 
 
 Parse.Cloud.define('sendPush', function(request, response) {
-    var username = request.params.username;
-    var message = request.params.message;
-    
+
     console.log(request.params);
-    response.success(request.params);
-//     Parse.Push.send({
-//         channels: [ request.params.channel ],
-//         data: {
-//             alert: message,
-//             sound: 'default',
-//             badge: 'Increment'
-//         }
-//     }, {
-//             success: function() {
-//                     // Push was successful
-//                     console.log('### PUSH SENT');
-//                     response.success("### Push sent");
-//             },
-//             error: function(error) {
-//                     // Handle error
-//                     console.log('### PUSH FAILED');
-//                     response.error("### Push failed: " + error);
-//             },
-//             useMasterKey: true
-//     });
+//     response.success(request.params);
+  
+    var targetUserId = request.params.targetUserId
+    var query = new Parse.Query(Parse.Installation);
+  
+    Parse.Push.send({
+
+      query.equalTo('owner', targetUserId);
+
+      where: query,
+      data: request.params
+    }, {
+      useMasterKey: true,
+      success: function() {
+        response.success("### Push sent")
+      },
+      error: function(error) {
+        response.error("### Push failed: " + error);
+      }
+    });
+    //     Parse.Push.send({
+    //         channels: [ request.params.channel ],
+    //         data: {
+    //             alert: message,
+    //             sound: 'default',
+    //             badge: 'Increment'
+    //         }
+    //     }, {
+    //             success: function() {
+    //                     // Push was successful
+    //                     console.log('### PUSH SENT');
+    //                     response.success("### Push sent");
+    //             },
+    //             error: function(error) {
+    //                     // Handle error
+    //                     console.log('### PUSH FAILED');
+    //                     response.error("### Push failed: " + error);
+    //             },
+    //             useMasterKey: true
+    //     });
 });
