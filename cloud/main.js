1fd5a9743f3e17cd36e88822b4893fda3c70153c
Francis Jervis <francis@timesfree.co>
2017-01-05 19:12:34 -0800

    Update main.js

--- cloud/main.js
+++ cloud/main.js
@@ -7,26 +7,27 @@ Parse.Cloud.define('hello', function(req, res) {
 Parse.Cloud.define('sendPush', function(request, response) {
     var username = request.params.username;
     var message = request.params.message;
-    console.log(request);
-
-    Parse.Push.send({
-        channels: [ request.params.channel ],
-        data: {
-            alert: message,
-            sound: 'default',
-            badge: 'Increment'
-        }
-    }, {
-            success: function() {
-                    // Push was successful
-                    console.log('### PUSH SENT');
-                    response.success("### Push sent");
-            },
-            error: function(error) {
-                    // Handle error
-                    console.log('### PUSH FAILED');
-                    response.error("### Push failed: " + error);
-            },
-            useMasterKey: true
-    });
+    
+    console.log(request.params);
+    response.success(request.params);
+//     Parse.Push.send({
+//         channels: [ request.params.channel ],
+//         data: {
+//             alert: message,
+//             sound: 'default',
+//             badge: 'Increment'
+//         }
+//     }, {
+//             success: function() {
+//                     // Push was successful
+//                     console.log('### PUSH SENT');
+//                     response.success("### Push sent");
+//             },
+//             error: function(error) {
+//                     // Handle error
+//                     console.log('### PUSH FAILED');
+//                     response.error("### Push failed: " + error);
+//             },
+//             useMasterKey: true
+//     });
 });
